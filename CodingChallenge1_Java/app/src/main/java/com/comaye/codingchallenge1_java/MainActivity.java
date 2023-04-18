31410dc566a0d9c890f6351c7ccca210941a55cd
somaye khaleghi <SomayeKhaleghi@gmail.com>
2023-04-19 03:09:41 +0330

    use DataBindingUtil
    bind  button's text, and progress bar's visibility programmatically, access to this views by DataBindingUtil

--- CodingChallenge1_Java/app/src/main/java/com/comaye/codingchallenge1_java/MainActivity.java
+++ CodingChallenge1_Java/app/src/main/java/com/comaye/codingchallenge1_java/MainActivity.java
@@ -1,14 +1,42 @@
 package com.comaye.codingchallenge1_java;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
 
+import com.comaye.codingchallenge1_java.databinding.ActivityMainBinding;
+//--------------------------------------------------------------------------------------------
 public class MainActivity extends AppCompatActivity {
 
+ActivityMainBinding binding;
+//--------------------------------------------------------------------------------------------
 @Override
 protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+  binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+  binding.button.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v)
+      {
+            setBinding();
+      }
+  });
 }
+//--------------------------------------------------------------------------------------------
+private  void setBinding()
+{
+    if (binding.button.getText().toString().equals(getString(R.string.start)))
+    {
+        binding.button.setText(getString(R.string.stop));
+        binding.progressBar.setVisibility(View.VISIBLE);
+    }
+    else
+    {
+        binding.button.setText(getString(R.string.start));
+        binding.progressBar.setVisibility(View.INVISIBLE);
+    }
+}
+//--------------------------------------------------------------------------------------------
+
 }
