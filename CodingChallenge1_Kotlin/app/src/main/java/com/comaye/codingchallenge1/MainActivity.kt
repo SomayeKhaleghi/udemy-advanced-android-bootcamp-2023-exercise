763be96d8ab6dff275bd97fd0090e3593a86b132
somaye khaleghi <SomayeKhaleghi@gmail.com>
2023-04-19 03:33:16 +0330

    write startOrStopProgressBar function

--- CodingChallenge1_Kotlin/app/src/main/java/com/comaye/codingchallenge1/MainActivity.kt
+++ CodingChallenge1_Kotlin/app/src/main/java/com/comaye/codingchallenge1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.comaye.codingchallenge1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.comaye.codingchallenge1.databinding.ActivityMainBinding
 //------------------------------------------------------------------------------------------
@@ -12,6 +13,22 @@ lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.button.setOnClickListener {  startOrStopProgressBar() }
     }
     //------------------------------------------------------------------------------------------
+    fun startOrStopProgressBar()
+    {
+        binding.apply {
+            if(button.text.equals(getString(R.string.start)))
+            {
+                progressBar.visibility = View.VISIBLE;
+                button.setText(getString(R.string.stop))
+            }
+            else
+            {
+                progressBar.visibility = View.INVISIBLE;
+                button.setText(getString(R.string.start))
+            }
+        }
+    }
 }
