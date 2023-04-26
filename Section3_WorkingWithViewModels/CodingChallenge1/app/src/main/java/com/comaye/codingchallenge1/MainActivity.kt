1955c673331588ce60a36be162127ea1f11e7322
somaye khaleghi <SomayeKhaleghi@gmail.com>
2023-04-26 21:53:00 +0330

    use dataBindingUtil
    in main activity, create binding variable to access to  it's views

--- Section3_WorkingWithViewModels/CodingChallenge1/app/src/main/java/com/comaye/codingchallenge1/MainActivity.kt
+++ Section3_WorkingWithViewModels/CodingChallenge1/app/src/main/java/com/comaye/codingchallenge1/MainActivity.kt
@@ -2,17 +2,20 @@ package com.comaye.codingchallenge1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import androidx.databinding.DataBindingUtil.setContentView
 import androidx.lifecycle.ViewModelProvider
-
+import com.comaye.codingchallenge1.databinding.ActivityMainBinding
+import androidx.databinding.DataBindingUtil.setContentView
 //------------------------------------------------------------------------------------------------------
 class MainActivity : AppCompatActivity() {
 //------------------------------------------------------------------------------------------------------
-    ///private lateinit var binding: ActivityMainBinding
+private lateinit var binding: ActivityMainBinding
+    //private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    binding =   setContentView(this, R.layout.activity_main)
 
-       // setContentView(R.layout.activity_main)
+
     }
 //------------------------------------------------------------------------------------------------------
 }
