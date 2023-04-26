017503a646f7cfaf6089c209471021df6960b120
somaye khaleghi <SomayeKhaleghi@gmail.com>
2023-04-26 23:24:11 +0330

    Update MainActivityViewModelFactory.kt

--- Section3_WorkingWithViewModels/CodingChallenge1/app/src/main/java/com/comaye/codingchallenge1/MainActivityViewModelFactory.kt
+++ Section3_WorkingWithViewModels/CodingChallenge1/app/src/main/java/com/comaye/codingchallenge1/MainActivityViewModelFactory.kt
@@ -4,15 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivityViewModelFactory(private val startingInput : Int):ViewModelProvider.Factory {
-
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-      //  super.create(modelClass)
-        if (modelClass.isAssignableFrom(MainActivityViewModel::class.java))
-        {
-            return MainActivityViewModel(startingInput) as T
-        }
-        throw  java.lang.IllegalArgumentException("Unknown class")
-
-        //return super.create(modelClass)
+//---------------------------------------------------------------------------------------------------------------
+override fun <T : ViewModel> create(modelClass: Class<T>): T {
+    if (modelClass.isAssignableFrom(MainActivityViewModel::class.java))
+    {
+        return MainActivityViewModel(startingInput) as T
     }
+    throw  java.lang.IllegalArgumentException("Unknown class")
+}
+//-----------------------------------------------------------------------------------------------------------------------
 }
