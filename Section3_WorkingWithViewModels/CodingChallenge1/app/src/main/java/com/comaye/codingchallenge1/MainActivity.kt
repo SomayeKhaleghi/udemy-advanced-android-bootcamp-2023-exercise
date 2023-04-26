a60617ab2f5a333b2c50c4de276817dbe52f63bc
somaye khaleghi <SomayeKhaleghi@gmail.com>
2023-04-26 22:20:46 +0330

    add functionality to view model class

--- Section3_WorkingWithViewModels/CodingChallenge1/app/src/main/java/com/comaye/codingchallenge1/MainActivity.kt
+++ Section3_WorkingWithViewModels/CodingChallenge1/app/src/main/java/com/comaye/codingchallenge1/MainActivity.kt
@@ -9,12 +9,20 @@ import androidx.databinding.DataBindingUtil.setContentView
 class MainActivity : AppCompatActivity() {
 //------------------------------------------------------------------------------------------------------
 private lateinit var binding: ActivityMainBinding
-    //private lateinit var viewModel: MainActivityViewModel
-
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    binding =   setContentView(this, R.layout.activity_main)
+        binding =   setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
+        binding.input.hint = (viewModel.GetInput().toString())
+        binding.output.text =viewModel.Add(binding.input.text.toString().toInt()).toString()
+
+        binding.button.setOnClickListener {
+            binding.output.text =viewModel.Add(binding.input.text.toString().toInt()).toString()
+            binding.input.hint = (viewModel.GetInput().toString())
+
+        }
 
     }
 //------------------------------------------------------------------------------------------------------
--- Section3_WorkingWithViewModels/CodingChallenge1/app/src/main/java/com/comaye/codingchallenge1/MainActivityViewModel.kt
+++ Section3_WorkingWithViewModels/CodingChallenge1/app/src/main/java/com/comaye/codingchallenge1/MainActivityViewModel.kt
@@ -2,7 +2,25 @@ package com.comaye.codingchallenge1
 
 import androidx.lifecycle.ViewModel
 class MainActivityViewModel: ViewModel() {
+//-----------------------------------------------------------------------------------------
+    private var input: Int = 0
+
+    private var output: Int = 0
+
+    //-----------------------------------------------------------------------------------------
+    fun GetInput(): Int {
+        return input
+    }
+
+    //-----------------------------------------------------------------------------------------
+    fun Add(input:Int): Int {
+
+        this.input  =input
+        output+=input
+
+        return output
+    }
 
 
-
+//-----------------------------------------------------------------------------------------
 }
