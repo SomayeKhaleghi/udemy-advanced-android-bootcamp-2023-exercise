fbaca15b3736ea19324f8725733ee9f468258980
somaye khaleghi <SomayeKhaleghi@gmail.com>
2023-04-26 21:43:53 +0330

    add views to main activity

--- Section3_WorkingWithViewModels/CodingChallenge1/app/src/main/java/com/comaye/codingchallenge1/MainActivity.kt
+++ Section3_WorkingWithViewModels/CodingChallenge1/app/src/main/java/com/comaye/codingchallenge1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.comaye.codingchallenge1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import androidx.databinding.DataBindingUtil.setContentView
+import androidx.lifecycle.ViewModelProvider
 
+//------------------------------------------------------------------------------------------------------
 class MainActivity : AppCompatActivity() {
+//------------------------------------------------------------------------------------------------------
+    ///private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+       // setContentView(R.layout.activity_main)
     }
+//------------------------------------------------------------------------------------------------------
 }
+//------------------------------------------------------------------------------------------------------
