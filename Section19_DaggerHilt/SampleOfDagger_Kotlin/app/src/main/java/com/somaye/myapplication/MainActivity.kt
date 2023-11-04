ed17b9ac1b96a4a7038343e5a80c3dd3d8dda5ea
somaye khaleghi <nicegirl0100@gmail.com>
2023-11-04 21:43:07 +0330

    call dagger component

--- Section19_DaggerHilt/SampleOfDagger_Kotlin/app/src/main/java/com/somaye/myapplication/MainActivity.kt
+++ Section19_DaggerHilt/SampleOfDagger_Kotlin/app/src/main/java/com/somaye/myapplication/MainActivity.kt
@@ -2,16 +2,13 @@ package com.somaye.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //SmartPhone(Battery(), SimCard(ServiceProvider()), MemoryCard() ).makeACallWithRecording()
 
 
-      //SmartPhone(Battery(), SimCard(ServiceProvider()), MemoryCard() ).makeACallWithRecording()
-
-
-
+        DaggerSmartPhoneComponent.create().getSmartPhone().makeACallWithRecording()
      }
 }
